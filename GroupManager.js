1970a81e205a79bb22069cd69a0877b23bd82189
SwartZCoding <seaflyscontact@gmail.com>
2022-11-08 11:25:07 +0100

    :art: Working on Group System

---
+++ GroupManager.js
@@ -0,0 +1,32 @@
+const groupList = new Array();
+
+function addGroup(creatorName, groupName) {
+    const group = new Group(groupList.length + 1, groupName, new Array(), creatorName);
+    groupList.push(group);
+}
+
+function deleteGroup(group) {
+    const objWithIdIndex = groupList.findIndex((obj) => obj.id === group.id);
+    groupList.splice(objWithIdIndex, 1);
+}
+
+function addUserToGroup(user, group) {
+    group.memberList.push(user.id);
+}
+
+function deleteUserToGroup(user, group) {
+    const objWithIdIndex = group.memberList.findIndex((obj) => obj.id === user.id);
+    group.memberList.splice(objWithIdIndex, 1);
+
+    // Gestion groupe.
+    if(group.getOwnerName == user.getName()) {
+        if(group.memberList.length == 0) {
+            deleteGroup(group);
+            console.log('Group' + group.getName() + " has been deleted.");
+        } else {
+            const random = Math.floor(Math.random() * group.memberList.length);
+            group.setOwnerName(group.memberList[random])
+            console.log('Group' + group.getName() + " has now a new owner named : " + group.getOwnerName);
+        }
+    }
+}
---
+++ datas/group.js
@@ -0,0 +1,34 @@
+class Group {
+
+    constructor(id, name, memberList, ownerName) {
+        this.id = id;
+        this.name = name;
+        this.memberList = memberList;
+        this.ownerName = ownerName;
+    }
+
+    get getId() {
+        return this.id;
+    }
+
+    get getName() {
+        return this.name;
+    }
+
+    set setName(newName) {
+        this.name = newName;
+    }
+
+    get getMemberList() {
+        return this.memberList;
+    }
+
+    get getOwnerName() {
+        return this.ownerName;
+    }
+
+    set setOwnerName(newName) {
+        this.ownerName = newName;
+    }
+
+}
