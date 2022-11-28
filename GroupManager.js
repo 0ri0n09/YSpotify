125d8b0d36a70f3a8b597320d657838c1138b0c2
KAlecs12 <alexandre.bruno@ynov.com>
2022-11-28 11:40:21 +0100

    Fix join/add Group

--- GroupManager.js
+++ GroupManager.js
@@ -5,11 +5,14 @@ const groupList = [];
 let nextGroupId = 0;
 
 function addGroup(creatorId, groupName) {
-    const group = new Group(nextGroupId++, groupName, [], creatorId);
+    const group = new Group(nextGroupId++, groupName, [creatorId], creatorId);
     groupList.push(group);
 
     let user = userManager.getUserById(creatorId);
-    joinGroup(user, group);
+    if (user.currentGroup != null) deleteUserToGroup(user.id, getGroupByID(user.currentGroup));
+    user.currentGroup = group.id;
+    console.log("Group créé pour " + user.nickname)
+
 }
 
 function deleteGroup(group) {
@@ -32,6 +35,12 @@ function joinGroup(userId, groupId) {
     const User = userManager.getUserById(userId);
     const Group = getGroupByID(groupId);
 
+    if(User.currentGroup == groupId) {
+        console.log(User)
+        console.log(groupId)
+        console.log("Cannot join the group you already joined.");
+    }
+
     if(User.currentGroup != groupId) {
         console.log("Delete old user group")
         console.log(User)
@@ -41,11 +50,6 @@ function joinGroup(userId, groupId) {
         User.currentGroup = groupId;
     }
 
-    if(User.currentGroup == groupId) {
-        console.log(User)
-        console.log(groupId)
-        console.log("Cannot join the group you already joined.");
-    }
 }
 
 function deleteUserToGroup(user, group) {
